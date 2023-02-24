0450a7d601c6996aba6ef9df67c1c3caebd9629d
nojuku <vluchnikov@yahoo.ca>
2023-02-24 09:59:11 -0500

    debugging particles

--- src/particle_system.js
+++ src/particle_system.js
@@ -1,81 +1,3 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { ImageLoader } from 'three'
-
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
-const debugObject = {}
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-/**f
- * Loaders
- */
-const gltfLoader = new GLTFLoader()
-
-
-
-/**
- * Environment map
- */
-const cubeTextureLoader = new THREE.CubeTextureLoader()
-const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
-
-environmentMap.encoding = THREE.sRGBEncoding
-
-debugObject.envMapIntensity = 1
-gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001)
-
-scene.background = environmentMap
-scene.environment = environmentMap
-
-/**
- * Models
- */
-gltfLoader.load(
-    '/models/baba_NEW_v2.glb',
-    (gltf) => {
-        gltf.scene.scale.set(10, 10, 10)
-        gltf.scene.position.set(0, - 4, 0)
-        gltf.scene.rotation.y = Math.PI * 0.5
-        scene.add(gltf.scene)
-        updateAllMaterials()
-        gui.add(gltf.scene.rotation, 'y').min(- Math.PI).max(Math.PI).step(0.001).name('rotation')
-    }
-)
-
-/**
- * Update all materials
- */
-const updateAllMaterials = () => {
-    scene.traverse((child) => {
-        if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
-            child.material.envMap = environmentMap
-            child.material.envMapIntensity = 2.5
-            child.material.envMapIntensity = debugObject.envMapIntensity
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
-}
-
 // VERTEX SHADER 
 const _VS = `
 uniform float pointMultiplier;
@@ -97,10 +19,6 @@ void main() {
   gl_FragColor = texture2D(diffuseTexture, coords) * vColour;
 }`;
 
-
-/**
- * PARTICLES
- */
 
 class LinearSpline {
     constructor(lerp) {
@@ -135,6 +53,8 @@ class LinearSpline {
     }
 }
 
+import * as THREE from 'three'
+
 class ParticleSystem {
     constructor(params) {
       const uniforms = {
@@ -157,7 +77,7 @@ class ParticleSystem {
           vertexColors: true
       });
   
-      this._camera = camera;
+      this._camera = params.camera;
       this._particles = [];
   
       this._geometry = new THREE.BufferGeometry();
@@ -168,7 +88,7 @@ class ParticleSystem {
   
       this._points = new THREE.Points(this._geometry, this._material);
   
-      scene.add(this._points);
+      params.parent.add(this._points);
   
       this._alphaSpline = new LinearSpline((t, a, b) => {
         return a + t * (b - a);
@@ -309,103 +229,3 @@ class ParticleSystem {
     }
 }
 
-
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-// Light 
-const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
-directionalLight.position.set(0.25, 3, - 2.25)
-directionalLight.castShadow = true
-directionalLight.shadow.camera.far = 15
-scene.add(directionalLight)
-
-gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
-gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001).name('lightX')
-gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001).name('lightY')
-gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001).name('lightZ')
-
-
-/**
- * Camera
- */
-
-// Base camera
-const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
-scene.add(directionalLightCameraHelper)
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
-scene.add(camera)
-
-
-/**
- * PARTICLE INIT
- */
-console.log('not working')
-fireParticles = new ParticleSystem();
-console.log('working')
-// scene.add(particles)
-
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.physicallyCorrectLights = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-
-// gui.add(renderer, 'toneMapping', {
-//     No: THREE.NoToneMapping,
-//     Linear: THREE.LinearToneMapping,
-//     Reinhard: THREE.ReinhardToneMapping,
-//     Cineon: THREE.CineonToneMapping,
-//     ACESFilmic: THREE.ACESFilmicToneMapping
-// })
-
-/**
- * Animate
- */
-const tick = () => {
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
