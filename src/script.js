3601b55e682ad3c9e9f18d2ef67d31c8f755039f
nojuku <vluchnikov@yahoo.ca>
2023-03-01 13:57:14 -0500

    Revert "fixing three import"
    This reverts commit c36e7a666549a6405535610a37d00a8e1edcbdc8.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import * as THREE from 'three'
 import * as dat from 'lil-gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
